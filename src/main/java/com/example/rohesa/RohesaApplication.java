f8cadd39894efc853f53d4aea0910a44c95b1498
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-09-14 17:32:12 +0700

    UPDATE: Update  Configuration

--- src/main/java/com/example/rohesa/RohesaApplication.java
+++ src/main/java/com/example/rohesa/RohesaApplication.java
@@ -1,13 +1,27 @@
 package com.example.rohesa;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
-public class RohesaApplication {
+@EnableScheduling
+public class RohesaApplication implements ApplicationRunner {
+	private static final Logger logger = LogManager.getLogger(RohesaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RohesaApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		logger.info("Version: " + SpringVersion.getVersion());
+		logger.info("Application Started ..");
+	}
 }
---
+++ src/main/java/com/example/rohesa/model/Jadwal.java
@@ -0,0 +1,45 @@
+package com.example.rohesa.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import java.util.Date;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "Jadwal")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Jadwal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "uid", nullable = false, unique = true)
+    private String uid = UUID.randomUUID().toString();
+
+    @Column(name = "req_description")
+    private String reqDescription;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "execution_type")
+    private String executionType;
+
+    @Column(name = "execution_at")
+    private Date executionAt;
+}
