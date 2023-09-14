cb36b57519b35a96b60071e46b17c876e82006b1
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-09-14 16:57:55 +0700

    ADD: First Commit

---
+++ src/main/java/com/example/rohesa/RohesaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rohesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RohesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rohesa/RohesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rohesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
